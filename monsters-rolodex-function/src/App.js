90fa5250a6aac588a5892bc8f984508280fb5a4a
Debjit Dhar <debjit8dhar@gmail.com>
2022-04-21 22:26:46 +0530

    created monsters list app with both functional and class component

---
+++ monsters-rolodex-function/src/App.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
+
+import "./App.css";
+
+const App = () => {
+  console.log("render");
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((monster) => setMonsters(monster));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        className="monsters-search-box"
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
+
+export default App;
---
+++ monsters-rolodex-function/src/components/card-list/card-list.component.jsx
@@ -0,0 +1,35 @@
+import { Component } from "react";
+
+import Card from "../card/card.component";
+import "./card-list.styles.css";
+
+const CardList = (props) => {
+    const { monsters } = props;
+
+    return (
+      <div className="card-list">
+        {monsters.map((monster) => {
+          return <Card monster={monster} />;
+        })}
+      </div>
+    );
+}
+
+/* class CardList extends Component {
+  render() {
+    console.log(this.props.monsters);
+    console.log("render from CardList");
+
+    const { monsters } = this.props;
+
+    return (
+      <div className="card-list">
+        {monsters.map((monster) => {
+          return <Card monster={monster} />;
+        })}
+      </div>
+    );
+  }
+} */
+
+export default CardList;
